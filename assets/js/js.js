34de665923a78cee64b1f02769ba83e8649838b5
Joey Reyes <jreyes88@gmail.com>
2016-05-28 10:14:08 -0500

    timers are working a little better

--- assets/js/js.js
+++ assets/js/js.js
@@ -29,12 +29,13 @@ var questions = [{
 // Empty gloval variables that the various functions need to manipulate to advance the game, determine the final score, and determine whether the game is over.
 var currentQuestion = 0;
 var correctAnswers = 0;
+var value;
 
 // All Game Functions
 $(document).ready(function () {
 
     // Hides the quizMessage div
-    // $(".quizMessage").hide();
+    $(".quizMessage").hide();
 
     // Displays start button
     $(".startButton").html("Start!");
@@ -46,6 +47,48 @@ $(document).ready(function () {
         game();
     });
 });
+
+// This function contains all of the game logic
+function game() {
+    $(".choiceList").find("li").remove();
+    displayCurrentQuestion();
+    runTimer();
+
+    // runs game logic when you click on the "Check Answer" button
+    $(".nextButton").on("click", function () {
+        stopTimer();
+
+        $(document).find(".quizContainer > .quizMessage").hide();
+
+        value = $("input[type='radio']:checked").val(); // this shit ain't working
+
+        // if the "Check Answer" button is clicked but no radio button is selected, then the Quiz Message appears instructing the player to select an answer
+        if (value == undefined) {
+            $(document).find(".quizMessage").text("Please select an answer");
+            $(document).find(".quizMessage").show();
+            $(".choiceList").find("li").remove();
+            displayCurrentQuestion();
+
+        // the following sections cover what to do when a radio button IS selected and the "Check Answer" button is clicked
+        } else {
+
+            // this section covers the condition in which the correct radio button is clicked
+            if (value == questions[currentQuestion].correctAnswer) {
+                $(document).find(".quizMessage").hide();
+                $(".nextButton").addClass("doNotDisplay");
+                correctAnswers++;
+                stopTimer();
+                advanceQuestion();
+
+            } else if (value !== questions[currentQuestion].correctAnswer) {
+                $(document).find(".quizMessage").hide();
+                $(".nextButton").addClass("doNotDisplay");
+                stopTimer();
+                advanceQuestion();
+            };
+        };
+    });
+};
 
 // This function displays the current question and the choices
 function displayCurrentQuestion() {
@@ -67,67 +110,37 @@ function displayCurrentQuestion() {
     };
 };
 
-// This function contains all of the game logic
-function game() {
-    
-    displayCurrentQuestion();
-
-    // hides the Quiz Message
-    $(this).find(".quizMessage").hide();
-
-
-    // runs game logic when you click on the "Check Answer" button
-    $(".nextButton").on("click", function () {
-
-    	$(document).find(".quizContainer > .quizMessage").hide();
-
-    	value = $("input[type='radio']:checked").val();
-
-        // if the "Check Answer" button is clicked but no radio button is selected, then the Quiz Message appears instructing the player to select an answer
-        if (value == undefined) {
-            $(document).find(".quizMessage").text("Please select an answer");
-            $(document).find(".quizMessage").show(); // why can't i make this disappear?
-            $(".choiceList").find("li").remove();
-            displayCurrentQuestion();
-
-        // the following sections cover what to do when a radio button IS selected and the "Check Answer" button is clicked
-        } else {
-
-        	// this section covers the condition in which the correct radio button is clicked
-            if (value == questions[currentQuestion].correctAnswer) {
-                correctAnswers++;
-            	advanceQuestion();
-
-            } else if (value !== questions[currentQuestion].correctAnswer) {
-                advanceQuestion();
-            };
-        };
-    });
-};
-
 // Run this function if time runs out
-
 function timesUp() {
 	$(".question").text("Time is up! The correct answer is " + questions[currentQuestion].correctChoice);
+    $(".nextButton").addClass("doNotDisplay");
 	runHiddenTimer();
+    stopTimer();
+    number = 0;
 };
 
 // Moves onto the next question. Resets the timer to 11
 function advanceQuestion() {
 	if (currentQuestion < questions.length - 1) {
+        $(document).find(".quizMessage").hide();
         currentQuestion++;
-        console.log(currentQuestion);
         $(".choiceList").find("li").remove();
-        displayCurrentQuestion();
+        game();
         number = 11;
         hiddenNumber = 5;
-        runTimer();
+        stopTimer();
+        stopHiddenTimer();
+        $(".nextButton").addClass("doNotDisplay");
     } else {
+        $(document).find(".quizMessage").hide();
         $(".choiceList").find("li").remove();
         $(".question").text("Game Over!");
-        stopTimer();
         $(".timer").addClass("doNotDisplay");
         displayScore();
+        number = 0;
+        hiddenNumber = 5;
+        stopTimer();
+        stopHiddenTimer();
     };
 };
 
@@ -136,6 +149,8 @@ function displayScore() {
     $(document).find(".quizContainer > .result").show();
     $(".nextButton").text("Try Again?").addClass("resetButton").removeClass("nextButton");
     tryAgainReset();
+    stopTimer();
+    stopHiddenTimer();
 };
 
 function tryAgainReset() {
@@ -144,6 +159,8 @@ function tryAgainReset() {
         $(".choiceList").find("li").remove();
         currentQuestion = 0;
         correctAnswers = 0;
+        number = 11;
+        hiddenNumber = 5;
         game();
         console.log(currentQuestion);
     });
@@ -166,9 +183,8 @@ function decrement(){
 	number --;
 	$(".timer").html('<h2>' + number + '</h2>');
 	if (number === 0){
-    stopTimer();
-    timesUp();
-    // alert('Time Up!')
+        timesUp();
+        $(".choiceList").find("li").remove();
 	};
 };
 
@@ -189,7 +205,7 @@ function hiddenDecrement(){
 	if (hiddenNumber === 0){
     stopHiddenTimer();
     advanceQuestion();
-    // alert('Time Up!')
+    game();
 	};
 };
 
